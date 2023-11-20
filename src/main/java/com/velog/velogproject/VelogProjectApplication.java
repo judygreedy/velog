e58c2fec8b7f9cd810b2a565a57c633367bf68f7
DiligentP <qkrwjdgus0603@gmail.com>
2023-11-20 22:19:41 +0900

    Delete Help.md

--- src/main/java/com/velog/velogproject/VelogProjectApplication.java
+++ src/main/java/com/velog/velogproject/VelogProjectApplication.java
@@ -2,12 +2,18 @@ package com.velog.velogproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VelogProjectApplication {
+public class VelogProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VelogProjectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VelogProjectApplication.class);
+	}
 }
